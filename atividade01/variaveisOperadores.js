ad1e09efb07a0aa7e0c55e4138d2039f7781a662
@yasmin.vitoria <yasmin.vitoria@digiteam.com.br>
2024-03-15 12:47:18 -0300

    atividade 01

---
+++ atividade01/variaveisOperadores.js
@@ -0,0 +1,17 @@
+var numA = 23;
+var numB = 5;
+
+var resultadoSoma = numA + numB;
+console.log("Resultado da adição entre " + numA + " e " + numB + ": " + resultadoSoma);
+
+var resultadoSubtracao = numA - numB;
+console.log("Resultado da subtração entre " + numA + " e " + numB + ": " + resultadoSubtracao);
+
+var resultadoMultiplicacao = numA * numB;
+console.log("Resultado da multiplicação entre " + numA + " e " + numB + ": " + resultadoMultiplicacao);
+
+var resultadoDivisao = numA / numB;
+console.log("Resultado da divisão entre " + numA + " e " + numB + ": " + resultadoDivisao);
+
+var resultadoResto = numA % numB;
+console.log("Resultado do resto entre " + numA + " e " + numB + ": " + resultadoResto);
