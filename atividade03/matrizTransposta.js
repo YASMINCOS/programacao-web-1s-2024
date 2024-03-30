ae44602c6e1f3c65b5a5ca8e7ac26ed41b14c4f4
YASMINCOS <costayasmin367@gmail.com>
2024-03-30 09:32:20 -0300

    a

---
+++ atividade03/matrizTransposta.js
@@ -0,0 +1,30 @@
+function transporMatriz(A) {
+    console.log("Matriz Original:");
+    imprimirMatriz(A);
+
+    let transposta = [];
+    for (let i = 0; i < A[0].length; i++) {
+        transposta.push([]);
+        for (let j = 0; j < A.length; j++) {
+            transposta[i].push(A[j][i]);
+        }
+    }
+
+    console.log("Matriz Transposta:");
+    imprimirMatriz(transposta);
+}
+
+function imprimirMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+        console.log(matriz[i].join("\t"));
+    }
+    console.log("");
+}
+
+const matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+transporMatriz(matriz);
